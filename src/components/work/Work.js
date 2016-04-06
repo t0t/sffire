7d97b3d1d3561310d23349da2683fc6708b988b7
Sergio Forés Raga <sergiofores@gmail.com>
2016-04-06 11:46:16 +0200

    :clap: Refactorizado

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -4,13 +4,17 @@ export default class Work extends Component {
   render() {
     return (
       <article className="Works__item">
-        <h4 className="Works__item__title">{this.props.title}</h4>
-        <figure className="Works__figure">
-          <img src={this.props.image} />
-          <figcaption className="Works__figure__caption">
-            {this.props.desc}
-          </figcaption>
-        </figure>
+        { (this.props.header) ?
+          <h4 className="Works__item__title">
+            {this.props.title}
+          </h4> : null }
+        { (this.props.image) ?
+          <figure className="Works__figure">
+            <img src={this.props.image} />
+            <figcaption className="Works__figure__caption">
+              {this.props.desc}
+            </figcaption>
+          </figure> : null }
       </article>
     );
   }
